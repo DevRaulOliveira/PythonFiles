2ad3ef1de9bdceb6154afb0fea7aaee011523520
Raul Oliveira <168892121+DevRaulOliveira@users.noreply.github.com>
2024-05-22 06:26:12 -0300

    Add files via upload

---
+++ PythonMaterial/[Python] Loop aninhado.py
@@ -0,0 +1,9 @@
+# Desafio 12  - Udemy
+# Para este desafio, crie uma lista de frutas e outra de vegerais. Use um "for loop" aninhado para imprimir todas as combinações de frutas e vegetais, com a fruta primeiro e o vegetal em segundo.
+
+frutas = ['maça','uva','manga']
+vegetais = ['alface', 'batata', 'pera']
+
+for frut in frutas: #ATENÇÃO COM OS NOMES DAS VARIÁVEIS
+    for veg in vegetais: #ATENÇÃO COM OS NOMES DAS VARIÁVEIS
+        print(frut, veg)

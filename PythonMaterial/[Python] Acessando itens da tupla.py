78328f63249c363c2ec7918c08e5acb1f66ebce4
Raul Oliveira <raul_cez@outlook.com>
2024-05-29 18:08:03 -0300

    File Add

---
+++ PythonMaterial/[Python] Acessando itens da tupla.py
@@ -0,0 +1,16 @@
+
+# Exemplo
+tupla = ('Gabriel', 'Danny', 'Arthur')
+print(tupla)
+
+# Forma de selecionar o item da tupla
+print('Acessando o item da tupla usando [] \n', tupla[0])
+
+# Acessando o ultimo item da tupla
+print('Acessando o ultimo item da tupla [-1]\n', tupla[-1])
+
+# Acessando o intervalo de itens
+# Caso queira seguir o conceito da lista, ele também é aplicado [:5] neste caso do indice 4 até o primeiro indice
+# Caso queira acessar a partir do primeiro fica dessa forma [0:]
+print('Acessando um intervalo de itens de uma tupla [:] \n', tupla[0:2])
+

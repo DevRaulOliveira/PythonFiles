f20f9e1d81de4d0f1c86d7ef304a66a41ecd55a4
Raul Oliveira <168892121+DevRaulOliveira@users.noreply.github.com>
2024-06-10 08:30:00 -0300

    [Udemy] GUI Tkinter

---
+++ PythonMaterial/[Python] Tkinter, conceito e introducao.py
@@ -0,0 +1,12 @@
+# Tkinter é uma ferramenta de elementos gráficos
+# Um conjunto de ferramentas gráficas, presente no Python
+# Contem Widgets das aplicações, como botões e barras de interface
+# Lembrando sempre de rodar utilizando o terminal Python
+
+# Modulo TKINTER
+import tkinter
+
+# Modelo de abertura do TKINKER
+tkinter._test()
+
+# Abrindo o terminal Python. ## Basta digitar Python no terminal e rodar o código nesse terminal específico.

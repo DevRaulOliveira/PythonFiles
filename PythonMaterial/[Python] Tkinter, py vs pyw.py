3d858de46bee624d0699b0c9855f05a4a29c719c
Raul Oliveira <raul_cez@outlook.com>
2024-06-10 21:07:26 -0300

    [Udemy] GUI Tkinter

---
+++ PythonMaterial/[Python] Tkinter, py vs pyw.py
@@ -0,0 +1,10 @@
+# Diferenças entre arquivos .py e .pyw
+# Arquivos .py precisam do terminal, e depois é executada a aplicação
+# Os arquivos .pyw abre apenas a aplicação
+# Caso queira realizar a edição da aplicação salva em formato .pyw, basta abrir com o bontão direito e selecionar o Vs Code
+
+import tkinter as tk # Importando o módulo tkinter
+
+root = tk.Tk() #Nome da guia do ambiente gráfico
+
+root.mainloop() # Loop de execução da aplicação

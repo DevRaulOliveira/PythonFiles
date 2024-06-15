6769a34a7c9b77912deb21786d7cafe82f9e7a17
Raul Oliveira <raul_cez@outlook.com>
2024-06-15 14:09:33 -0300

    [Udemy] GUI Tkinter

--- PythonMaterial/[Python] Tkinter, alterando o icone da aplicacao.py
+++ PythonMaterial/[Python] Tkinter, alterando o icone da aplicacao.py
@@ -1,5 +1,5 @@
 # Como alterar o icone da aplicação
-
+# O arquivo precisa estar no formato .ico
 import tkinter as tk
 
 root = tk.Tk()
@@ -12,5 +12,8 @@ center_x = int(root.winfo_screenwidth()/2 - window_width/2)
 center_y = int(root.winfo_screenheight()/2 - window_heigh/2)
 
 root.geometry(f'{window_width}x{window_heigh}+{center_x}+{center_y}')
+
+# Modelo de alteração de icone, utilizando o metodo ICONBITMAP
+root.iconbitmap('C:\\Users\\Raul\\Downloads\\Icone.ico')
 
 root.mainloop()
